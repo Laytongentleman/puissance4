6564122833a3500f4ec423ae019d0e676a1c17a9
FRANCHI Gabriel <gabriel.franchi@ism-antony.org>
2021-02-02 12:24:42 +0100

    fini

--- puissance4_Cylia_Gabriel_Louis.py
+++ puissance4_Cylia_Gabriel_Louis.py
@@ -22,7 +22,7 @@ def display(g):
     cg = init(7,6)
     for i in range(len(g)):
         for j in range(len(g[i])):
-            cg[i][j] = "O" if g[i][j] == 1 else "X" if g[i][j] == 2  else 0
+            cg[i][j] = "O" if g[i][j] == 1 else "X" if g[i][j] == 2  else " "
     for i in range(len(g[0])):
         for j in range(len(g)):
             print(cg[j][i], end=" | ")
@@ -34,7 +34,6 @@ def coup_possible(g, c):
     if len(g)-1 < c or g[c][0] != 0:
         return False
     return True
-
 
 
 #verif
@@ -61,14 +60,51 @@ def horiz (g, j, l, c):
             point+=1
         else:
             point=0
-        print(point, l,i)
         if point==4:
             return True
 
-def diag (g, j, l, c):
+def diagd (g, j, l, c):
     point=0
-    for i in range (6):
-        pass
+    t = max(l,c) - min(l,c)
+    coord = [0,0] # 0 c, 1 ;l
+    if l > c :
+        coord [1] = t
+    else:
+        coord [0] = t
+    while True:
+        if g[coord [0]][coord [1]]==1 and j:
+            point+=1
+        elif g[coord [0]][coord [1]]==2 and not j:
+            point+=1
+        else:
+            point=0
+        if point==4:
+            return True
+        coord[0] += 1
+        coord[1] += 1
+        if coord[0] > 6 or coord[1] > 5:
+            return False
+
+def diagm (g, j, l, c):
+    point=0
+    coord = [c,l]
+    while coord[0] >= 0 and coord[1] < 5:
+        coord[0] -= 1
+        coord[1] += 1
+    while True:
+        if g[coord [0]][coord [1]]==1 and j:
+            point+=1
+        elif g[coord [0]][coord [1]]==2 and not j:
+            point+=1
+        else:
+            point=0
+
+        if point==4:
+            return True
+        coord[0] += 1
+        coord[1] -= 1
+        if coord[0] > 6 or coord[1]<0:
+            return False
 
 
 def check(g,j,l,c):
@@ -76,15 +112,21 @@ def check(g,j,l,c):
         print("Le joueur", int(j)+1, "a gagné par vertical !")
     if horiz(g,j,l,c):
         print("Le joueur", int(j)+1, "a gagné par horizontal !")
-    if diag (g,j,l,c):
-        print ("Le joueur", int(j)+1, "a gagné par diagonal !")
+    if diagd(g,j,l,c):
+        print ("Le joueur", int(j)+1, "a gagné par diagonal Descendante!")
+    if diagm(g,j,l,c):
+        print ("Le joueur", int(j)+1, "a gagné par diagonal Montante!")
+    if diagm(g,j,l,c) or  diagd(g,j,l,c) or vertical(g,j,l,c) or horiz(g,j,l,c):
+        return True
+
 
 
 def coup_aléatoire (g, j):
     c = randint (0, len(g)-1)
     while not coup_possible (g, c):
         c = randint (0, len(g))
-    jouer (g, j, c)
+    if jouer(g, j, c):
+        return True
 
 
 
@@ -96,28 +138,44 @@ def fin (g):
     return False
 
 
+def select():
+    mode = input("alea, 2j")
+    return mode
+
 # main script
 def main():
-    """fait tourner le jeu"""
-    a = randint (0,1)
     j = True
-    if a == 0:
-        j = not j
-    while fin(g):
-        if j:
-            coup_aléatoire (g, j)
+    if select() == "alea":
+        """fait tourner le jeu contre un bot aléatoire"""
+        a = randint (0,1)
+        if a == 0:
+            j = not j
+        while fin(g):
+            if j:
+                if coup_aléatoire (g, j):
+                    print("Perdu")
+                    return True
+                j =  not j
+                if not fin(g):
+                    break
+            print("_____________________________")
+            display(g)
+            c = int(input("colonne?"))
+            if jouer(g, j, c):
+                return True
             j =  not j
-            if not fin(g):
-                break
-        print("_____________________________")
-        display(g)
-        c = int(input("colonne?"))
-        jouer(g, j, c)
-        j =  not j
+    if select() == "2j":
+        """fait tourner le jeu en local à 2 joueurs"""
+        while fin(g):
+            print("_____________________________")
+            display(g)
+            c = int(input("colonne?"))
+            j =  not j
+            if jouer(g, j, c):
+                return True
     print("_____________________________")
     display(g)
     print ("Le tableau est complet !")
-
 
 
 
@@ -128,10 +186,10 @@ def jouer(g,j,c):
         for i in range(len(g[c])-1,-1, -1):
             if g[c][i] == 0:
                 g[c][i] = 1 if j else 2
-                check(g,j,i,c)
+                if check(g,j,i,c):
+                    return True
                 break
     else:
-        print("c'est complet")
         jouer(g,j, int(input("recommence")))
 
 
