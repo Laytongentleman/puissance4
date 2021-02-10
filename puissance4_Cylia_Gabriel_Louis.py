62b4f48c26e331200747cfc4320987e43b8d6994
Cylia LOUIS <cylia.louis@gmail.com>
2021-02-10 17:29:24 +0100

    VictoireReorganisation

--- puissance4_Cylia_Gabriel_Louis.py
+++ puissance4_Cylia_Gabriel_Louis.py
@@ -1,8 +1,8 @@
 from random import randint
 
+# ------------- Fonctions qui gèrent la grille -------------------
 
-#initialise
-
+# initialise la grille
 def init(l, h):
     """ prends la longueur et hauteur du tableau
     et renvoie toujours un tableau à 2 dimensions rempli de 0"""
@@ -10,12 +10,6 @@ def init(l, h):
     g = [ [0] * h for i in range(l)]
     return g
 
-# Soit g notre grille de jeu
-g = init(7,6)
-
-
-
-# ------------- Fonctions qui sont appelées en jeu -------------------
 
 def display(g):
     """fonction d'affichage qui affiche dans le bon sens le tableau
@@ -38,22 +32,20 @@ def display(g):
         print()
 
 
-def coup_possible(g, c):
-    """détermine si l'on peut jouer dans la colonne:
-        - si la colonne demandée existe dans g
-        - si la colonne n'est pas totalement remplie """
-    if  c < len(g) and c >= 0:
-        if g[c][0] == 0:
-            return True
-    else:
-        return False
 
 
-#------------ Fonctions de vérification ------------------------
+#------------ Fonctions de vérification de fin de jeu ------------------------
+
+def fin (g):
+    """détermine si la grille est pleine"""
+    for i in range (len(g)):
+        if coup_possible(g, i):
+            return False
+    return True
 
 def vertical (g,j,l,c):
     """ vérifie la colonne où le coup est joué """
-    point=0
+    point = 0
     for i in range (6):
         if g[c][i]== 1 and j:
             point+=1
@@ -63,7 +55,6 @@ def vertical (g,j,l,c):
             point=0
         if point==4:
             return True
-
 
 
 def horiz (g, j, l, c):
@@ -125,7 +116,7 @@ def diagm (g, j, l, c):
             return False
 
 
-def check(g,j,l,c):
+def check (g,j,l,c):
     """ fonction qui exécute toutes les vérifications"""
     gagner = "" # Variable qui permet d'afficher quelle vérification est vérifiée
     if vertical(g,j,l,c):
@@ -143,6 +134,35 @@ def check(g,j,l,c):
 
 
 
+# -------------- Fonctions qui gèrent le coup du joueur ou du bot -----------------
+
+def coup_possible (g, c):
+    """détermine si l'on peut jouer dans la colonne:
+        - si la colonne demandée existe dans g
+        - si la colonne n'est pas totalement remplie """
+    print(type(c))
+    print (c)
+    if  c < len(g) and c >= 0:
+        if g[c][0] == 0:
+            return True
+    return False
+
+
+def choix_colonne (g):
+    """demande au joueur la colone dans la quelle il veut jouer quand qu'il n'a pas donnée un nombre possible"""
+    c = None
+    j = None
+    while j == None and str(j) != c:
+        c = input("colonne? ")
+        for i in range (len(g)):
+            if c == str(i):
+                j = i
+    while not coup_possible (g, j):
+        # fait rejouer le joueur si celui-ci a demandé une colonne pleine
+        print ("La colonne est pleine. ")
+        j = choix_colonne(g)
+    return j
+
 def coup_aléatoire (g, j):
     """fonction qui joue un coup aléatoire pour le joueur entré en argument """
     c = randint (0, len(g)-1)
@@ -151,50 +171,87 @@ def coup_aléatoire (g, j):
     if jouer(g, j, c):
         return True
 
-
-
+def jouer (g,j,c):
+    """joue le coup à partir du joueur, de la grille et de la colonne"""
+    for i in range(len(g[c])-1,-1, -1):
+        if g[c][i] == 0:
+            g[c][i] = 1 if j else 2
+            if check(g,j,i,c):
+                return True
+            return False
 
 
 # ---------------- fonctions principales pour faire tourner le jeu ---------------------
 
+# Pour démarer une partie ou quitter le je
+def jeu ():
+    """ Gère le début et la fin du jeu"""
 
-def fin (g):
-    """détermine si la grille est pleine"""
-    for i in range (len(g)):
-        if coup_possible(g, i):
-            return False
-    return True
+    # Permet le comtage des points
+    point = [0,0,0,0,0,0]
+    recommencer = input("Voulez-vous jouer à puissance 4 ? oui, non : ")
+    while recommencer != "non":
+
+        if recommencer == "oui":
+            # initialise une grille pour le jeu
+            g = init (7, 6)
+
+            select(g, point)
+
+        print ("Merci d'avoir jouer")
+        recommencer = input("Recommencer? oui, non : ")
+
+    # Affiche le nombre de victoire
+    if point[-1] != 0:
+        victoire (point)
+
+        # Politesse de fin
+        print ("Merci d'avoir jouer. ")
+    print ("A une prochaine fois. ")
+
+def victoire (point):
+    """Affiche le nombre de victoire par joueur selon le mode de jeu, ainsi que le nombre de partie"""
+
+    if point[1] != 0 or point[2] != 0:
+        print ()
+        print ("Le premier joueur a gagné", point[1], "fois. ")
+        print ("Le deuxième joueur a gagné", point[2], "fois. ")
+
+    if point[3] != 0 or point[4] != 0:
+        print ()
+        print ("Le bot a perdu", point[3], "fois. ")
+        print ("Le bot a gagné", point[4], "fois. ")
+
+    print ()
+    print ("Le nombre de partie nule est de :", point[0])
+    print ("Le nombre totale de partie est de :", point[-1])
+    print ()
 
 
-def select(g):
+# Permet d'avoir plusieurs modes de jeu
+def select (g, point):
     """permet de choisir le mode de jeu"""
     mode = 0
     while mode != "alea" and mode != "2j":
         mode = input("Mode ? alea, 2j : ")
 
+    # compte le nombre de partie
+    point[-1] += 1
+
     if mode == "alea":
-        g = main_alea(g)
+        point[main_alea(g)] += 1
     if mode == "2j":
-        g = main_2j(g)
+        point[main_2j(g)] += 1
 
     # En cas d'égalité
     if fin(g):
         display(g)
         print ("Le tableau est complet !")
 
-    recommencer = 0
-    while recommencer != "oui" and recommencer != "non":
-        recommencer = input("Recommencer? oui, non : ")
-    if recommencer == "non":
-        print ("Merci d'avoir jouer")
-    elif recommencer == "oui":
-        gn = init (7, 6)
-        select(gn)
-
 
 
 # main script contre un bot
-def main_alea(g):
+def main_alea (g):
     """fait tourner le jeu contre un bot aléatoire"""
     #choix arbitraire du joueur modifiable
     j = True
@@ -207,66 +264,41 @@ def main_alea(g):
         if j:
             if coup_aléatoire (g, j):
                 print("Perdu")
-                return g
+                return 4
             j =  not j
             if fin(g):
-                return g
+                return 0
         display(g)
 
         c = choix_colonne(g)
         if jouer(g, j, c):
-            return g
+            return 3
         j =  not j
-    return g
+    return 0
 
 # main script pour 2 joueurs
-def main_2j(g):
+def main_2j (g):
     """fait tourner le jeu en local à 2 joueurs"""
-    #choix arbitraire du joueur modifiable
+    # choix arbitraire du joueur modifiable
     j = True
     while not fin(g):
         display(g)
         c = choix_colonne(g)
         j =  not j
         if jouer(g, j, c):
-            return g
-    return g
+            return int(j)+1
+    return 0
 
 
-def jouer(g,j,c):
-    """joue le coup à partir du joueur, de la grille et de la colonne"""
-
-    if coup_possible(g,c):
-
-        # si le coup est possible prendre la 1er case vide en partant du bas
-        for i in range(len(g[c])-1,-1, -1):
-            if g[c][i] == 0:
-                g[c][i] = 1 if j else 2
-                if check(g,j,i,c):
-                    return True
-                break
-    else:
-        # fait rejouer le joueur si celui-ci a demandé un coup impossible
-        print ("La colonne est pleine, recommence. ")
-        jouer(g,j, choix_colonne(g))
-
-def choix_colonne (g):
-    c = input("colonne? ")
-    for i in range (len(g)):
-        if c == str(i):
-            c = int (c)
-            return c
-    choix_colonne(g)
-
-
-
-
+"""
+g = init(7,6)
 assert coup_possible(g, len(g)) == False , "Erreur d'index trop grand"
 assert coup_possible(g, -1) == False , "Erreur d'index trop petit"
 assert fin([[1,1],[1,1]]) == True , "Erreur de fin"
 assert fin([[1,0],[0,1]]) == False , "Erreur de fin"
+"""
 
-print(coup_possible(g, 0))
+jeu()
 
-select(g)
+
 
